8d9d1b3314e63a623ed64e2eabd492e0cbcc6086
Andrey Linko <AndreyLinko@gmail.com>
2023-04-25 21:05:24 +0300

    feat: init mikro-orm request context module

---
+++ packages/nestjs-mikro-orm-request-context/src/interceptors/mikro-orm-request-context.interceptor.ts
@@ -0,0 +1,36 @@
+import type { NestInterceptor }  from '@nestjs/common'
+import type { ExecutionContext } from '@nestjs/common'
+import type { CallHandler }      from '@nestjs/common'
+
+import { RequestContext }        from '@mikro-orm/core'
+import { MikroORM }              from '@mikro-orm/core'
+import { Injectable }            from '@nestjs/common'
+
+import { Observable }            from 'rxjs'
+
+@Injectable()
+export class MikroORMRequestContextInterceptor implements NestInterceptor {
+  constructor(private readonly orm: MikroORM) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return new Observable((subscriber) => {
+      RequestContext.createAsync(
+        this.orm.em,
+        () =>
+          new Promise<void>((resolve) => {
+            next.handle().subscribe(
+              (result) => {
+                subscriber.next(result)
+                subscriber.complete()
+                resolve()
+              },
+              (error) => {
+                subscriber.error(error)
+                resolve()
+              }
+            )
+          })
+      )
+    })
+  }
+}

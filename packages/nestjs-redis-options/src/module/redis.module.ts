a16cbf8ed7811e7f273cdee512974004a03d3d99
Andrey Linko <AndreyLinko@gmail.com>
2023-05-27 23:30:46 +0300

    feat: redis module factory

--- packages/nestjs-redis-options/src/module/redis.module.ts
+++ packages/nestjs-redis-options/src/module/redis.module.ts
@@ -2,28 +2,22 @@ import type { DynamicModule }            from '@nestjs/common'
 import type { RedisOptions }             from 'ioredis'
 
 import { Module }                        from '@nestjs/common'
-import { Redis }                         from 'ioredis'
 
 import { REDIS_MODULE_OPTIONS_HOST }     from './redis.module.constants.js'
 import { REDIS_MODULE_OPTIONS_PORT }     from './redis.module.constants.js'
 import { REDIS_MODULE_OPTIONS_PASSWORD } from './redis.module.constants.js'
 import { REDIS_MODULE_OPTIONS_USERNAME } from './redis.module.constants.js'
 import { RedisConfigFactory }            from './redis.config-factory.js'
+import { RedisFactory }                  from './redis.factory.js'
 
 @Module({})
 export class RedisModule {
   static register(options: RedisOptions = {}): DynamicModule {
-    const provider = {
-      provide: Redis,
-      inject: [RedisConfigFactory],
-      useFactory: (configFactory) => new Redis(configFactory.createRedisOptions()),
-    }
-
     return {
       module: RedisModule,
       providers: [
-        provider,
         RedisConfigFactory,
+        RedisFactory,
         {
           provide: REDIS_MODULE_OPTIONS_HOST,
           useValue: options.host,
@@ -41,7 +35,7 @@ export class RedisModule {
           useValue: options.password,
         },
       ],
-      exports: [provider, RedisConfigFactory],
+      exports: [RedisConfigFactory, RedisFactory],
     }
   }
 }

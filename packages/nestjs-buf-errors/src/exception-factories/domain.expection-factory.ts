ffbbedaa890eda355ee59969e9bb7e715e9a28d9
Andrey Linko <AndreyLinko@gmail.com>
2023-09-02 17:25:06 +0300

    feat: init connectrpc modules

--- packages/nestjs-buf-errors/src/exception-factories/domain.expection-factory.ts
+++
@@ -1,8 +0,0 @@
-import type { DomainError } from '@monstrs/core-errors'
-
-import { Code }             from '@bufbuild/connect'
-import { ConnectError }     from '@bufbuild/connect'
-import { RpcException }     from '@nestjs/microservices'
-
-export const domainExceptionFactory = (error: DomainError): RpcException =>
-  new RpcException(new ConnectError(error.message, Code.InvalidArgument))
---
+++ packages/nestjs-connectrpc-errors/src/exception-filters/connectrpc.exception-filter.ts
@@ -0,0 +1,39 @@
+import type { ArgumentsHost }         from '@nestjs/common'
+import type { Observable }            from 'rxjs'
+
+import { AssertionError }             from 'node:assert'
+
+import { Catch }                      from '@nestjs/common'
+import { BaseRpcExceptionFilter }     from '@nestjs/microservices'
+import { GuardErrors }                from '@monstrs/guard-clause'
+import { DomainError }                from '@monstrs/core-errors'
+
+import { ValidationError }            from '@monstrs/nestjs-validation'
+
+import { assertionExceptionFactory }  from '../exception-factories/index.js'
+import { validationExceptionFactory } from '../exception-factories/index.js'
+import { guardExceptionFactory }      from '../exception-factories/index.js'
+import { domainExceptionFactory }     from '../exception-factories/index.js'
+
+@Catch()
+export class ConnectRpcExceptionsFilter extends BaseRpcExceptionFilter {
+  override catch(exception: unknown, host: ArgumentsHost): Observable<any> {
+    if (exception instanceof AssertionError) {
+      return super.catch(assertionExceptionFactory(exception), host)
+    }
+
+    if (exception instanceof DomainError) {
+      return super.catch(domainExceptionFactory(exception), host)
+    }
+
+    if (exception instanceof GuardErrors) {
+      return super.catch(guardExceptionFactory(exception), host)
+    }
+
+    if (exception instanceof ValidationError) {
+      return super.catch(validationExceptionFactory(exception.errors), host)
+    }
+
+    return super.catch(exception, host)
+  }
+}

1e452297bd38d8966064107feca739fcab263706
Andrey Linko <AndreyLinko@gmail.com>
2023-09-03 14:43:58 +0300

    feat: s3 client async module

--- packages/nestjs-s3-client/src/module/s3-client.config-factory.ts
+++ packages/nestjs-s3-client/src/module/s3-client.config-factory.ts
@@ -1,32 +1,26 @@
 import type { S3ClientConfig }        from '@aws-sdk/client-s3'
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import type { S3ClientModuleOptions } from './s3-client.module.interfaces.js'
 
-import { AwsCredentialIdentity }      from '@aws-sdk/types'
 import { Inject }                     from '@nestjs/common'
 import { Injectable }                 from '@nestjs/common'
 import { fromEnv }                    from '@aws-sdk/credential-providers'
 
-import { S3_CLIENT_ENDPOINT }         from './s3-client.module.constants.js'
-import { S3_CLIENT_REGION }           from './s3-client.module.constants.js'
-import { S3_CLIENT_CREDENTIALS }      from './s3-client.module.constants.js'
+import { S3_CLIENT_MODULE_OPTIONS }   from './s3-client.module.constants.js'
 
 @Injectable()
 export class S3ClientConfigFactory {
   constructor(
-    @Inject(S3_CLIENT_ENDPOINT)
-    private readonly endpoint?: string,
-    @Inject(S3_CLIENT_REGION)
-    private readonly region?: string,
-    @Inject(S3_CLIENT_CREDENTIALS)
-    private readonly credentials?: AwsCredentialIdentity
+    @Inject(S3_CLIENT_MODULE_OPTIONS)
+    private readonly options: S3ClientModuleOptions
   ) {}
 
   createS3ClientOptions(options: S3ClientModuleOptions = {}): S3ClientConfig {
     return {
-      endpoint: options.endpoint || this.endpoint || process.env.S3_ENDPOINT,
-      region: options.region || this.region || process.env.S3_REGION,
-      credentials: options.credentials || this.credentials || fromEnv(),
+      endpoint: options.endpoint || this.options.endpoint || process.env.S3_ENDPOINT,
+      region: options.region || this.options.region || process.env.S3_REGION,
+      credentials: options.credentials || this.options.credentials || fromEnv(),
       forcePathStyle: true,
     }
   }

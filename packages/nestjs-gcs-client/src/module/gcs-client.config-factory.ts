3f9772240a7fd1c4506e403408090fadc2f9aa54
Andrey Linko <AndreyLinko@gmail.com>
2023-09-03 14:51:42 +0300

    feat: gcs client async module

--- packages/nestjs-gcs-client/src/module/gcs-client.config-factory.ts
+++ packages/nestjs-gcs-client/src/module/gcs-client.config-factory.ts
@@ -1,26 +1,24 @@
 import type { StorageOptions }         from '@google-cloud/storage'
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import type { GcsClientModuleOptions } from './gcs-client.module.interfaces.js'
 
 import { Injectable }                  from '@nestjs/common'
 import { Inject }                      from '@nestjs/common'
 
-import { GCS_CLIENT_API_ENDPOINT }     from './gcs-client.module.constants.js'
-import { GCS_CLIENT_KEY_FILENAME }     from './gcs-client.module.constants.js'
+import { GCS_CLIENT_MODULE_OPTIONS }   from './gcs-client.module.constants.js'
 
 @Injectable()
 export class GcsClientConfigFactory {
   constructor(
-    @Inject(GCS_CLIENT_API_ENDPOINT)
-    private readonly apiEndpoint?: string,
-    @Inject(GCS_CLIENT_KEY_FILENAME)
-    private readonly keyFilename?: string
+    @Inject(GCS_CLIENT_MODULE_OPTIONS)
+    private readonly options: GcsClientModuleOptions
   ) {}
 
   createGcsClientOptions(options: GcsClientModuleOptions = {}): StorageOptions {
     return {
-      apiEndpoint: options.apiEndpoint || this.apiEndpoint || process.env.GCS_API_ENDPOINT,
-      keyFilename: options.keyFilename || this.keyFilename || process.env.GCS_KEY_FILENAME,
+      apiEndpoint: options.apiEndpoint || this.options.apiEndpoint || process.env.GCS_API_ENDPOINT,
+      keyFilename: options.keyFilename || this.options.keyFilename || process.env.GCS_KEY_FILENAME,
     }
   }
 }
--- packages/nestjs-gcs-client/src/module/gcs-client.module.constants.ts
+++ packages/nestjs-gcs-client/src/module/gcs-client.module.constants.ts
@@ -1,2 +1,1 @@
-export const GCS_CLIENT_API_ENDPOINT = Symbol('gcs-client-api-endpoint')
-export const GCS_CLIENT_KEY_FILENAME = Symbol('gcs-client-key-filename')
+export const GCS_CLIENT_MODULE_OPTIONS = Symbol('gcs-client-module-options')
